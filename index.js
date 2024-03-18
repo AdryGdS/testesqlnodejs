9ee21d55ba56c5fdf0f5072697724d4da1173e7e
Adriely Gogora <99200566+AdryGdS@users.noreply.github.com>
2024-03-18 23:26:52 +0000

    Código7.2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Configuração do MySQL
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: process.env.MYSQL_USER || "root", // Usa variáveis de ambiente
   password: process.env.MYSQL_PASSWORD || "mamae123",
   database: process.env.MYSQL_DATABASE || "userdb"
